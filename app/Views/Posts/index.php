f417f1c95c9a8d8db85a065b206c23141a7b8933
nabilaahptr <nabilaahptr@gmail.com>
2021-10-21 16:33:12 +0700

    Praktikum Web Lanjut

--- app/Views/Posts/index.php
+++ app/Views/Posts/index.php
@@ -1,8 +1,7 @@
-<?= $this->extend('template'); ?>
-<?= $this->section('content'); ?>
+<?= $this->extend('template');?>
 
+<?= $this->section('content');?>
 <body class="hold-transition sidebar-mini layout-fixed">
-
 <div class="wrapper">
 
   <!-- Preloader -->
@@ -186,21 +185,22 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item menu-open">
-            <a href="/admin" class="nav-link active">
+            <a href="/admin" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
-                <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <li class="nav-item menu">
+          </li>
+          <li class="nav-item">
             <a href="/admin/posts" class="nav-link">
-              <i class="nav-icon fas "></i>
+              <i class="nav-icon fas fa-book-open"></i>
               <p>
-                My Post
-                <i class="right fas fa-angle-left"></i>
+                My Posts
               </p>
             </a>
+          </li>
+            
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
@@ -215,7 +215,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
+            <h1 class="m-0">My Posts</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -227,13 +227,51 @@
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-    <!-- /.main content -->
+
     <div class="container">
-        <a href="/admin/posts/create" class="btn btn-primary"><i class="fas fa-plus"></i>Tambah Data</a>
-    </div>
-    <!-- /.main content -->
-          <!-- right col -->
+        <a href="/admin/posts/create" class="btn btn-primary"> <i class="fas fa-plus"></i>Tambah Data</a>
+        <div class="card mt-3">
+          <div class="card-header">
+            Daftar Postingan
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped text-center">
+                  <thead>
+                    <tr>
+                      <th scope="col">No.</th>
+                      <th scope="col">Judul</th>
+                      <th scope="col">Slug</th>
+                      <th scope="col">Author</th>
+                      <th scope="col">Kategori</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php foreach($posts as $i => $post) : ?>
+                    <tr>
+                      <th scope="row"><?= $i +1; ?></th>
+                      <td><?= $post ['judul'] ?></td>
+                      <td><?= $post ['slug'] ?></td>
+                      <td><?= $post ['author'] ?></td>
+                      <td><?= $post ['kategori'] ?></td>
+                      <td>
+                          <a href="/posts/edit/<?= $post['slug'];?>" class="btn btn-warning me-1"> <i class="fas fa-edit"></i> Edit </a>
+                          <a href="/posts/delete/<?= $post['slug'];?>" class="btn btn-danger "> <i class="fas fa-trash"></i> Delete </a>
+                      </td>
+                    </tr>
+                    <?php endforeach;?>
+                  </tbody>
+                </table>
+            </div>
+          </div>
         </div>
+    
+    </div>
+
+    <!-- Main content -->
+    <section class="content">
+      
         <!-- /.row (main row) -->
       </div><!-- /.container-fluid -->
     </section>
@@ -256,4 +294,4 @@
 </div>
 <!-- ./wrapper -->
 
-<?= $this->endSection(); ?>
+<?= $this->endSection();?>
